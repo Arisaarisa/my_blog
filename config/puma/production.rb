5be506011a635b724d957fbe0b92e79de272bf8e
arisa <arisankochira10@gmail.com>
2019-07-20 15:02:13 +0900

    miss

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,4 +1,5 @@
 environment "production"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -11,8 +12,10 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 
+
 # Specifies the `environment` that Puma will run in.
 #
+
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -32,4 +35,4 @@ threads threads_count, threads_count
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
   bind "unix:#{Rails.root}/tmp/sockets/puma.sock"
-  daemonize ture
+  daemonize true
